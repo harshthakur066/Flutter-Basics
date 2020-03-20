211f941539d03549db6dbd48da861af0af1c5aff
harshthakur066 <harshthakur066@gmail.com>
2020-03-21 00:09:57 +0530

    changed stateless to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,21 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   var quetionIndex = 0;
 
   void answerQuestions() {
-    quetionIndex++;
+    setState(() {
+      quetionIndex++;
+    });
     print(quetionIndex);
   }
 
