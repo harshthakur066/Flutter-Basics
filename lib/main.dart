8eb02af39b9c32d6396f4bcf3cdca09c1291cacb
harshthakur066 <harshthakur066@gmail.com>
2020-03-20 23:58:55 +0530

    buttons click functions

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var quetionIndex = 0;
+
+  void answerQuestions() {
+    quetionIndex++;
+    print(quetionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
       'What\'s your fav. color?',
       'What\'s yuor fav. animal?',
     ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -20,21 +28,24 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The Questions'),
-            Row(children: [
-              RaisedButton(
-                child: Text('Ans 1'),
-                onPressed: null,
-              ),
-              RaisedButton(
-                child: Text('Ans 2'),
-                onPressed: null,
-              ),
-              RaisedButton(
-                child: Text('Ans 3'),
-                onPressed: null,
-              ),
-            ]),
+            Text(
+              questions[quetionIndex],
+            ),
+            RaisedButton(
+              child: Text('Ans 1'),
+              onPressed: answerQuestions,
+            ),
+            RaisedButton(
+              child: Text('Ans 2'),
+              onPressed: () => print('Ans 2 choosen!'), //Anonymus Function
+            ),
+            RaisedButton(
+              child: Text('Ans 3'),
+              onPressed: () {
+                //...More line codes
+                print('Ans 3 choosen!');
+              },
+            ),
           ],
         ),
       ),
